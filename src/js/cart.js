a00f3bc9c36873e5f2392b510f7b52670f8ac923
Lendherm <122584856+Lendherm@users.noreply.github.com>
2025-05-09 19:34:16 -0600

    update
    Empty Card Error: cart.html
    
    Update the renderCartContents() function in cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,12 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];  // Fallback a []
+  if (!Array.isArray(cartItems)) return;               // Protección extra
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">

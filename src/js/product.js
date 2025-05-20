bf14c924b0a21f895844cccd6f1f38f0f8008494
Anna Abbott <aabbott@byupathway.edu>
2025-05-19 20:54:18 -0500

    fixed bug adding product to cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,13 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
+  let cartItems = getLocalStorage("so-cart", "[]");
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
+  const id = e.target.dataset.id;
+  console.log(`addToCartHandler - id: ${id}`);
   const product = await dataSource.findProductById(e.target.dataset.id);
+  console.log(`addToCartHandler - product`, product);
+
   addProductToCart(product);
 }
 

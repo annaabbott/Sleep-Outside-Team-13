cc62526df4e8de1565df833ec4e34d6bbed3e7e6
Anna Abbott <aabbott@byupathway.edu>
2025-05-24 14:40:42 -0500

    dynamic product data cards working

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,21 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+loadHeaderFooter();
+
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
+
 const productDetails = new ProductDetails(productId, dataSource);
 productDetails.init();
 
-// Test the parameter
-// console.log("Product ID:", productId);
+//////////////////////////////////////////////////////////////////////////
 
 // Test product lookup
 dataSource
   .findProductById(productId)
-  // .then((productItem) => console.log("Product Details:", productItem))
+  .then((productItem) => console.log("Product Details:", productItem))
   .catch((error) => console.error(error));
 
 // Existing cart functionality remains unchanged

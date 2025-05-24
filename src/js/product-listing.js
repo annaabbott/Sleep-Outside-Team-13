0118f4b7032df2d510af26046e17746adece354e
Anna Abbott <aabbott@byupathway.edu>
2025-05-24 14:51:20 -0500

    completed dynamic product listing page

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -8,3 +8,14 @@ const dataSource = new ProductData();
 const element = document.querySelector(".product-list"); 
 const categoryList = new ProductList(category, dataSource, element);
 categoryList.init();
+
+const productCategory = document.querySelector("#product-category");
+
+const categoryNames = {
+    tents: "Tents",
+    backpacks: "Backpacks",
+    "sleeping-bags": "Sleeping Bags",
+    hammocks: "Hammocks"
+}
+
+productCategory.textContent = categoryNames[category];

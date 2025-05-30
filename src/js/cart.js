e5ed7a6dd183c0ef800443af84e96489dabb5265
Lendherm <122584856+Lendherm@users.noreply.github.com>
2025-05-30 16:54:02 -0600

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -10,21 +10,32 @@ function renderCartContents() {
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
 
   return newItem;
 }
 
-renderCartContents();
+function updateCartCount() {
+  const cartCountElement = document.getElementById('cart-count');
+  const cartItems = getLocalStorage('so-cart') || [];
+  const itemCount = cartItems.length;
+
+  cartCountElement.textContent = itemCount;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderCartContents();
+  updateCartCount();
+});

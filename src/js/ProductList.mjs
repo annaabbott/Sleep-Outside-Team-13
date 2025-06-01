746697ae3896f267a18d063fc869cc462ecb4b77
Anna Abbott <aabbott@byupathway.edu>
2025-06-01 14:23:26 -0500

    Added quick-view modal to product list page

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,4 +1,4 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { addProductToCart, renderListWithTemplate } from "./utils.mjs";
 
 //   <li class="product-card">
 //     <a href="product_pages/marmot-ajax-3.html">
@@ -28,6 +28,22 @@ function productCardTemplate(product) {
             <h2 class="card__name">${product.DescriptionHtmlSimple}</h2>
             <p class="product-card__price">\$${product.SuggestedRetailPrice}</p>
         </a>
+        <button type="button" id="modalBtn_${product.Id}" class="modalBtn">Quick view</button>
+        <dialog id="quickview_${product.Id}" class="quickview">
+          <section id="product-info">
+          <img 
+                src="${product.Images.PrimaryMedium}" 
+                alt="Image of ${product.Brand.Name}">
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.NameWithoutBrand}</h2>
+
+            <p id="p-color">${product.Colors[0].ColorName}</p>
+            <p id="p-description">${product.DescriptionHtmlSimple}</p>
+            <p id="p-price">\$${product.SuggestedRetailPrice}</p>
+            <button id="addToCart_${product.Id}" class="modalBtn">Add to Cart</button>
+            <button type="button" id="closeModal_${product.Id}" class="modalBtn" >Close</button>
+          </section>
+        </dialog>
     </li>`;
 }
 
@@ -45,7 +61,29 @@ export default class ProductList {
     document.querySelector("#product-category").textContent = this.category;
   }
 
+  addTemplateEventListeners(product) {
+    const dialogElement = document.querySelector(`#quickview_${product.Id}`);
+    const modalBtn = document.querySelector("#modalBtn_" + product.Id);
+    const closeModalBtn = document.querySelector(`#closeModal_${product.Id}`);
+    const addToCartBtn = document.querySelector(`#addToCart_${product.Id}`);
+
+    modalBtn.addEventListener("click", () => {
+      dialogElement.showModal();
+    });
+
+    closeModalBtn.addEventListener("click", () => {
+      dialogElement.close();
+    });
+
+    addToCartBtn.addEventListener("click", () => {
+      addProductToCart(product);
+    })
+  }
+
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
+    for (let i = 0; i < list.length; i++) {
+      this.addTemplateEventListeners(list[i]);
+    }
   }
 }
